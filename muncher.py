0523e220a49b0edc3a090faffee49d157377a7a8
dwunger <106967706+dwunger@users.noreply.github.com>
2023-06-12 05:03:22 -0400

    progress not matching

--- muncher.py
+++ muncher.py
@@ -1,19 +1,29 @@
 import os
 import subprocess
 import re
-out = subprocess.check_output(['fsutil', 'fsinfo', 'drives'])
-out = out.decode('utf-8')
-out = [item.strip() for item in out.split() if re.match(r'[A-Za-z]{1}:\\', item) is not None]
-print(out)
 
 
 
 
 
-# class manifest(self, id, name, ):
+class manifest(self, id, name, ):
 
 
-# class Muncher(self):
-#     def __init__(self, manifests, drives)
-#         self.manifests = load_manifests(drives)
-#     def load_manifests(drives)
+class Muncher(self):
+    def __init__(self, manifests, drives):
+        self.manifests = load_manifests(drives)
+        self.drives = get_disks()
+    def get_disks(): 
+        '''['C:\\', 'D:\\']'''
+        stdout_raw = subprocess.check_output(['fsutil', 'fsinfo', 'drives'])
+        std_utf8 = stdout_raw.decode('utf-8')
+        system_drives = [item.strip() for item in system_drives.split() if re.match(r'[A-Za-z]{1}:\\', item) is not None]
+        choice = input(f"Select a drive:\n{[f'{idx - 1}: {drive}' for idx, drive in enumerate(system_drives)]}\nDefault: All mounted drives (Enter)\n")
+        if choice == '':
+            return system_drives
+        else:
+            if choice.isdigit():
+                return system_drives[choice]
+    
+    def load_manifests(self.drives):
+        pass
